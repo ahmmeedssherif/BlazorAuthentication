817a6e2465a05a611adccd96bc52fe5fcd9c9311
Ahmed Sherif <ahmedsherif@Ahmeds-MacBook-Pro.local>
2024-07-22 06:47:43 +0300

    Blazor Authentication

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp1.Components;
+using BlazorApp1.Components.Account;
+using BlazorApp1.Data;
+using BlazorApp1.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<EmailSender>();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
---
+++ Services/EmailSender.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly string _fromEmail = "ahmmmeedsherif@outlook.com";
+        private readonly string _fromPassword = "1LoinLoin$$212";
+        private readonly string _smtpHost = "smtp-mail.outlook.com";
+        private readonly int _smtpPort = 587;
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var message = new MailMessage();
+            message.From = new MailAddress(_fromEmail);
+            message.Subject = subject;
+            message.To.Add(email);
+            message.Body = $"<html><body><p>{htmlMessage}</p></body></html>";
+            message.IsBodyHtml = true;
+
+            using (var smtpClient = new SmtpClient(_smtpHost, _smtpPort) {Credentials = new NetworkCredential(_fromEmail, _fromPassword), EnableSsl = true }) 
+            { 
+                await smtpClient.SendMailAsync(message);
+            }
+        }
+    }
+}
